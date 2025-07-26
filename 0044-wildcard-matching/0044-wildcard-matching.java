33b870eccf8b3c6725f9922d27ef2af45728fac4
dhivyan <dhivyan1104@gmail.com>
2025-07-26 23:59:21 +0530

    Time: 2 ms (97.14%), Space: 44.2 MB (91.79%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -1,36 +1,27 @@
 public class Solution {
     public boolean isMatch(String s, String p) {
-        int m = s.length(), n = p.length();
-        boolean[] prev = new boolean[n + 1];
-        boolean[] curr = new boolean[n + 1];
-
-        prev[0] = true;
-
-        for (int j = 1; j <= n; j++) {
-            if (p.charAt(j - 1) == '*') {
-                prev[j] = prev[j - 1];
+        int i = 0, j = 0, starIdx = -1, match = 0;
+        while (i < s.length()) {
+            if (j < p.length() && (p.charAt(j) == '?' || p.charAt(j) == s.charAt(i))) {
+                i++;
+                j++;
+            } else if (j < p.length() && p.charAt(j) == '*') {
+                starIdx = j;
+                match = i;
+                j++;
+            } else if (starIdx != -1) {
+                j = starIdx + 1;
+                match++;
+                i = match;
+            } else {
+                return false;
             }
         }
 
-        for (int i = 1; i <= m; i++) {
-            curr[0] = false;  // empty pattern cannot match non-empty string
-            for (int j = 1; j <= n; j++) {
-                char sc = s.charAt(i - 1);
-                char pc = p.charAt(j - 1);
-
-                if (pc == '*') {
-                    curr[j] = curr[j - 1] || prev[j];
-                } else if (pc == '?' || pc == sc) {
-                    curr[j] = prev[j - 1];
-                } else {
-                    curr[j] = false;
-                }
-            }
-            boolean[] temp = prev;
-            prev = curr;
-            curr = temp;
+        while (j < p.length() && p.charAt(j) == '*') {
+            j++;
         }
 
-        return prev[n];
+        return j == p.length();
     }
 }
