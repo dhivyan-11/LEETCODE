a15a3ac44cdebb3c5b0924746a912f890bfd9f56
dhivyan <dhivyan1104@gmail.com>
2025-07-26 23:56:34 +0530

    Time: 16 ms (79.54%), Space: 48.8 MB (14.22%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -1,32 +1,33 @@
 class Solution {
     public boolean isMatch(String s, String p) {
-        int m=s.length() ,n =p.length();
-        boolean [][]dp=new boolean[m+1][n+1];
-        dp[0][0]=true;
-
-        for(int j=1;j<=n;j++){
-            if(p.charAt(j-1)=='*'){
-                dp[0][j]=dp[0][j-1];
+        Boolean dp[][]=new Boolean[s.length()][p.length()];
+        // for(int i=0;i<s.length();i++)
+        //     Arrays.fill(dp[i],-1);
+       return  Match(s,p,s.length()-1,p.length()-1,dp);
+    }
+    public static boolean Match(String str,String pattern,int len1,int len2,Boolean[][]dp){
+        if(len2>=0 && len1<0)
+        {
+            // System.out.println("gh");
+            for(int j=0;j<=len2;j++){
+                if(pattern.charAt(j)!='*')
+                    return false;
             }
+            return true;
         }
-
-        for(int i=1;i<=m;i++){
-            for(int j=1;j<=n;j++){
-                char sc=s.charAt(i-1);
-                char pc=p.charAt(j-1);
-
-                if(sc==pc || pc=='?'){
-                    dp[i][j]=dp[i-1][j-1];
-               
-                }
-                 else if(pc=='*'){
-                    dp[i][j]=dp[i-1][j] || dp[i][j-1];
-                 }
-            }
+        if(len2<0 && len1>=0){
+            // System.out.println("gh");
+            return false;
         }
-
-
-        return dp[m][n];
-        
+        if(len1<=0 && len2<0)
+            return true;
+        if(dp[len1][len2]!=null)
+            return dp[len1][len2];
+        if(pattern.charAt(len2)=='?'||pattern.charAt(len2)==str.charAt(len1))
+            return dp[len1][len2]=Match(str,pattern,len1-1,len2-1,dp);
+        if(pattern.charAt(len2)=='*')
+            return dp[len1][len2]= Match(str,pattern,len1-1,len2,dp) || Match(str,pattern,len1,len2-1,dp);
+        //this states that the 2 char are not matching and they are not * and ? too
+        return dp[len1][len2]=false;
     }
 }
